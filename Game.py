7a51af07ea24bc673705eead1b7ae15248070bf8
Matthew Gallagher <mg95@hotmail.co.uk>
2023-01-26 23:56:34 +0000

    Add insurance logic and rework chip placement

--- Game.py
+++ Game.py
@@ -8,35 +8,63 @@ class Game:
         self.deck = Deck()
         self.player = Player()
         self.dealer = Dealer()
-        self.bet = 0
+        self.pot = 0
+
+    def display_info(self):
+        print("---Blackjack---")
+        print()
+        print("Dealer stands on 17")
+        print("Insurance pays 3:2")
+        print()
+        print("You have " + str(self.player.chips) + " chips")
+        print()
 
     def play(self):
+
+        self.pot = self.player.place_bet()
         self.deal_cards()
-        print()
+        self.check_blackjack()
         self.hit_or_stand()
 
-    def get_bet_amount(self):
-        try:
-            bet = int(input("Enter bet amount: "))
-        except ValueError:
-            print("Please enter a number.")
-            self.get_bet_amount()
-        return bet
+    def check_player_blackjack(self):
+        if self.player.hand.total == 21:
+            print(self.dealer.hand.cards[1])
+            if self.dealer.hand.total == 21:
+                print("Push")
+            else:
+                print("Blackjack!")
+                self.pot *= 2.5
+
+    def check_dealer_blackjack(self):
+        if self.dealer.hand.cards[0].rank == 1:
+            cost = self.pot // 2
+            print("Dealer has an Ace")
+            print("Insurance costs " + str(cost) + " chips")
+            insurance = self.player.place_insurance(cost)
+            if self.dealer.hand.total == 21:
+                print("Dealer has Blackjack!")
+                self.pot = insurance * 3
+                self.pay_out()
+
+    def pay_out(self):
+        print(f"You win {self.pot}")
+        self.player.add_chips(self.pot)
 
     def deal_cards(self):
-        self.player.place_bet(self.get_bet_amount())
-        print("Player\t\t\tDealer")
         self.player.hand.add_card(self.deck.deal())
-        print(self.player.hand.cards[0], end="\t\t")
         self.dealer.hand.add_card(self.deck.deal())
+        self.player.hand.add_card(self.deck.deal())
+        self.dealer.hand.add_card(self.deck.deal())
+        print("Player's hand: ")
+        for card in self.player.hand.cards:
+            print(card)
+        print(f"Player total: {self.player.hand.total}")
+
+        print("Dealer's hand: ")
         print(self.dealer.hand.cards[0])
-        self.player.hand.add_card(self.deck.deal())
-        print(self.player.hand.cards[1], end="\t\t")
-        self.dealer.hand.add_card(self.deck.deal())
-        print("_______________")
+        print("----------")
 
     def hit_or_stand(self):
-        print(f"Player total: {self.player.hand.total}")
 
         choice = input("Hit or Stand? (h/s): ")
         if choice == "h":
@@ -57,4 +85,5 @@ class Game:
 
 if __name__ == "__main__":
     game = Game()
+    game.display_info()
     game.play()
--- Player.py
+++ Player.py
@@ -6,12 +6,29 @@ class Player:
         self.hand = Hand()
         self.chips = 100
 
-    def place_bet(self, bet):
-
-        if bet > self.chips:
-            print("You don't have enough chips to place that bet.")
+    def place_chips(self, chips):
+        if chips > self.chips:
+            print("Can't place + " + str(chips) + " chips")
+            print("You only have " + str(self.chips) + " chips")
         else:
-            self.chips -= bet
+            self.chips -= chips
+            return chips
+
+    def choose_bet(self):
+        try:
+            bet = int(input("Enter bet amount: "))
+        except ValueError:
+            print("Please enter an integer")
+            self.choose_bet()
+        return bet
+
+    def place_insurance(self, cost):
+        choice = input("Place insurance? (y/n): ")
+        if choice == "y":
+            return self.place_chips(cost)
+        else:
+            print("No insurance")
+            return 0
 
     def add_chips(self, chips):
         self.chips += chips
